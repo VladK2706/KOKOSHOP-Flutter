40e2f32a27e5a7248404626831f381e14dd087b9
molan018 <stivenbohorquezmolano@gmail.com>
2024-09-16 11:14:13 -0500

    Update Carrito.dart

--- modelo/Carrito.dart
+++ modelo/Carrito.dart
@@ -1,23 +1,19 @@
-class Carrito{
-  int? ID;
-  int id_carrito;
+class Carrito {
+  int? id_carrito;  // Cambiado a opcional
   int id_cli;
 
   Carrito({
-    this.ID,
-    required this.id_carrito,
+    this.id_carrito,  // Ya no es requerido
     required this.id_cli,
-});
+  });
 
-factory Carrito.fromMap(Map<String, dynamic> json) => Carrito(
-  ID: json['ID'],
-  id_carrito: json['id_carrito'],
-  id_cli:  json['id_cli']
-);
+  factory Carrito.fromMap(Map<String, dynamic> json) => Carrito(
+      id_carrito: json['id_carrito'],
+      id_cli: json['id_cli']
+  );
 
-  Map<String, dynamic> toMap()=>{
-    'ID' : ID,
-    'id_carrito' : id_carrito,
-    'id_cli' : id_cli,
+  Map<String, dynamic> toMap() => {
+    'id_carrito': id_carrito,
+    'id_cli': id_cli,
   };
 }
